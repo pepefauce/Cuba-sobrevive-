a357079637cec02767101ef7ca1b7bc407820754
pepefauce <pepefauce.bot@gmail.com>
2026-09-23 01:47:36 -0400

    Initialize App component with FirstPersonGame

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FirstPersonGame from './components/FirstPersonGame';
+
+export default function App() {
+  return (
+    <main className="w-full min-h-screen bg-slate-950 flex items-center justify-center">
+      <FirstPersonGame />
+    </main>
+  );
+}
